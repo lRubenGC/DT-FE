8ed405e0b3560ba5733b1bfc6b44646c5b8bd208
rdevgc <rubengc1799@gmail.com>
2024-10-06 20:59:21 +0200

    1: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DOCUMENT, isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'DT-FE';
+  private readonly platform = inject(PLATFORM_ID);
+  private readonly document = inject(DOCUMENT);
+
+  constructor() {
+    if (isPlatformBrowser(this.platform)) {
+      console.warn('browser');
+      // Use document
+    }
+    if (isPlatformServer(this.platform)) {
+      console.warn('server');
+      // Use document injected (this.document)
+    }
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,27 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import {
+  HttpRequest,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        filter: (req: HttpRequest<unknown>) => true,
+        includeHeaders: [],
+        includePostRequests: true,
+      })
+    ),
+    provideHttpClient(withFetch()),
+  ],
 };
