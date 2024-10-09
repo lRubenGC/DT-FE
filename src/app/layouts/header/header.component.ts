1dd0787837cfa1e918f501992a046388fce4a952
rdevgc <rubengc1799@gmail.com>
2024-10-09 22:18:51 +0200

    DEV-9: HEADER

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslocoDirective } from '@jsverse/transloco';
+import { DcButtonComponent } from '@shared/components/dc-button/dc-button.component';
 
 @Component({
   selector: 'dt-header',
   standalone: true,
-  imports: [],
+  imports: [TranslocoDirective, DcButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
