66f014ac944779e0fd8c5b6bb6bdd301d4dba499
rdevgc <rubengc1799@gmail.com>
2024-12-10 23:25:24 +0100

    DEV-25: CLICK OUTSIDE DIRECTIVE

---
+++ src/app/shared/directives/click-outside-directive.ts
@@ -0,0 +1,24 @@
+import {
+  Directive,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appClickOutside]',
+  standalone: true,
+})
+export class ClickOutsideDirective {
+  @Output() clickOutside = new EventEmitter<void>();
+
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  protected onDocumentClick(event: MouseEvent): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.clickOutside.emit();
+    }
+  }
+}
