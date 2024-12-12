3698b711de2acf3e26274b0d3d08958289c41b65
rdevgc <rubengc1799@gmail.com>
2024-12-12 19:43:32 +0100

    DEV-44: REQUEST BUTTON USAGE

--- src/app/modules/basic-cars/components/basic-car-card/basic-car-card.component.ts
+++ src/app/modules/basic-cars/components/basic-car-card/basic-car-card.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { BasicCarDTO } from '@modules/basic-cars/models/basic-cars.models';
 import { DtButtonComponent } from '@shared/components/dt-button/dt-button.component';
+import { DtRequestButtonComponent } from '@shared/components/dt-request-button/dt-request-button.component';
 import { ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'dt-basic-car-card',
   standalone: true,
-  imports: [CommonModule, DtButtonComponent],
+  imports: [CommonModule, DtButtonComponent, DtRequestButtonComponent],
   templateUrl: './basic-car-card.component.html',
   styleUrl: './basic-car-card.component.scss',
 })
