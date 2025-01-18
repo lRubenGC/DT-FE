9f9c510c5ea5758dab1a977fff845a2aa3f2b1e9
rdevgc <rubengc1799@gmail.com>
2025-01-18 18:15:47 +0100

    DEV-111: IMAGE VIEWER

---
+++ src/app/shared/components/dt-image-overlay/components/image-viewer.component.ts
@@ -0,0 +1,14 @@
+import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-image-viewer',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-viewer.component.html',
+  styleUrl: './image-viewer.component.scss',
+})
+export class ImageViewerComponent {
+  public dialogRef = inject(DialogRef<{ a: number }, { b: number }>);
+  public src: string = inject(DIALOG_DATA);
+}
