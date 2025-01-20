93e5cdc489cee3d646da36baf19cc16f584d2846
rdevgc <rubengc1799@gmail.com>
2025-01-20 09:05:56 +0100

    DEV-114: DT BUTTON ACCEPTS LINKS

--- src/app/shared/components/dt-button/dt-button.component.ts
+++ src/app/shared/components/dt-button/dt-button.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { BehaviorSubject } from 'rxjs';
 
@@ -33,13 +34,13 @@ export type SAFELIST_COLORS =
 @Component({
   selector: 'dt-button',
   standalone: true,
-  imports: [CommonModule, AngularSvgIconModule],
+  imports: [CommonModule, AngularSvgIconModule, RouterLink],
   templateUrl: './dt-button.component.html',
   styleUrl: './dt-button.component.scss',
 })
 export class DtButtonComponent {
   //#region INPUTS
-  @Input({ required: true }) type!: 'icon' | 'button';
+  @Input({ required: true }) type!: 'icon' | 'button' | 'link';
   @Input() header: string = '';
   @Input() icon: DT_ICONS | null = null;
   @Input() title: string | null = null;
@@ -48,6 +49,9 @@ export class DtButtonComponent {
   @Input() headerClass: string = '';
   @Input() iconFill: string = '#202020';
   @Input() iconSize: string = '24px';
+  @Input() href: string = '';
+  @Input() hrefId: number = 0;
+  @Input() target: '_self' | '_blank' = '_self';
   @Input({ alias: 'loading' }) set loadingSetter(v: boolean | null) {
     if (v === null) return;
     this.loading$.next(v);
