7ec4f871ba7da619f1e7c439fbaa2eb7f6ca72f0
rdevgc <rubengc1799@gmail.com>
2025-01-21 09:04:23 +0100

    DEV-125: INFO PAGES START

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,24 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from '@auth/page/auth.component';
+import { BasicCarComponent } from '@modules/basic-cars/pages/basic-car/basic-car.component';
 import { BasicCarsComponent } from '@modules/basic-cars/pages/basic-cars/basic-cars.component';
 import { PremiumCarsComponent } from '@modules/premium-cars/pages/premium-cars/premium-cars.component';
 import { SpecialCarsComponent } from '@modules/special-cars/pages/special-cars/special-cars.component';
 import { HomeComponent } from './home/page/home/home.component';
-import { BasicCarComponent } from '@modules/basic-cars/pages/basic-car/basic-car.component';
+import { AboutComponent } from './info/about/about.component';
+import { ContactComponent } from './info/contact/contact.component';
+import { PoliciesComponent } from './info/policies/policies.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'auth', component: AuthComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'basic-cars', component: BasicCarsComponent },
   { path: 'basic-cars/:id', component: BasicCarComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'special-cars', component: SpecialCarsComponent },
+  { path: 'policies', component: PoliciesComponent },
   { path: 'premium-cars', component: PremiumCarsComponent },
   { path: '**', redirectTo: '/home' },
 ];
---
+++ src/app/info/policies/policies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dt-policies',
+  standalone: true,
+  imports: [],
+  templateUrl: './policies.component.html',
+  styleUrl: './policies.component.scss',
+})
+export class PoliciesComponent {}
