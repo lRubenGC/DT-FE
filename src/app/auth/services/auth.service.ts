9dc157a13bf26612050822b182b2100fd141f157
rdevgc <rubengc1799@gmail.com>
2025-01-22 12:29:10 +0100

    DEV-135: USER PROFILE OBSERVABLE

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
 import { UserDTO } from '@auth/models/auth.models';
 import { CrudService } from '@shared/services/crud/crud.service';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService extends CrudService {
+  private getUserProfile() {
+    return this.post<string, UserDTO>('/api/auth/get-user-profile', '');
+  }
+
+  public userProfile$: Observable<UserDTO | null> = this.getUserProfile().pipe(
+    map((resp) => (resp.ok ? resp.data : null)),
+    shareReplay({ refCount: true, bufferSize: 1 }),
+  );
+
   public login(payload: { email: string; password: string }) {
     return this.post<{ email: string; password: string }, UserDTO>(
       '/api/auth/login',
