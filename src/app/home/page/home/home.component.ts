5d74e6e6975514b95ec42de937f811159669b491
rdevgc <rubengc1799@gmail.com>
2025-01-22 14:39:23 +0100

    DEV-141: HOME PAGE (WIP)

--- src/app/home/page/home/home.component.ts
+++ src/app/home/page/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '@auth/services/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { DISCORD_INVITATION } from '@shared/models/urls.constants';
+import { DtButtonComponent } from '../../../shared/components/dt-button/dt-button.component';
+import { HomeCarCardComponent } from '../../components/home-car-card/home-car-card.component';
 
 @Component({
-  selector: 'app-home',
+  selector: 'dt-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    DtButtonComponent,
+    HomeCarCardComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  public readonly DISCORD_INVITATION = DISCORD_INVITATION;
+  private readonly authService = inject(AuthService);
+  public userProfile$ = this.authService.userProfile$;
 }
