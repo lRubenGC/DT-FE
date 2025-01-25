78ec8e2e43489be4fee7990e0606062326517202
rdevgc <rubengc1799@gmail.com>
2025-01-25 17:34:47 +0100

    DEV-152: GET USER PROFILE TRIGGERS WHEN ROUTE CHANGES

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,22 +1,29 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { UserDTO } from '@auth/models/auth.models';
 import { CrudService } from '@shared/services/crud/crud.service';
-import { map, Observable, share } from 'rxjs';
+import { filter, map, Observable, share, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService extends CrudService {
+  //#region USER PROFILE
+  private readonly router = inject(Router);
+  private onRouterChange$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+  );
   private getUserProfile() {
     return this.post<string, UserDTO>('/api/auth/get-user-profile', '', {
       skipErrorsInterceptor: true,
     });
   }
-
-  public userProfile$: Observable<UserDTO | null> = this.getUserProfile().pipe(
+  public userProfile$: Observable<UserDTO | null> = this.onRouterChange$.pipe(
+    switchMap(() => this.getUserProfile()),
     map((resp) => (resp.ok ? resp.data : null)),
     share(),
   );
+  //#endregion USER PROFILE
 
   public login(payload: { email: string; password: string }) {
     return this.post<{ email: string; password: string }, UserDTO>(
