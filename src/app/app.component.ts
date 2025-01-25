3af39fa155929390034bb9beb04870c7c7971278
rdevgc <rubengc1799@gmail.com>
2025-01-25 20:53:48 +0100

    DEV-163: LANG SAVED IN COOKIES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { DOCUMENT, isPlatformBrowser, isPlatformServer } from '@angular/common';
-import { Component, inject, PLATFORM_ID } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LanguageService } from '@shared/services/language/language.service';
+import {
+  DT_LANGS,
+  LanguageService,
+} from '@shared/services/language/language.service';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 import { ErrorToastComponent } from './errors/components/error-toast/error-toast.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { HeaderComponent } from './layouts/header/header.component';
@@ -21,21 +24,20 @@ import { SeoComponent } from './seo/seo.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  private readonly platform = inject(PLATFORM_ID);
-  private readonly document = inject(DOCUMENT);
-  // private readonly cookie = inject(SsrCookieService);
+  // private readonly platform = inject(PLATFORM_ID);
+  // private readonly document = inject(DOCUMENT);
+  private readonly cookie = inject(SsrCookieService);
   private readonly language = inject(LanguageService);
 
   constructor() {
-    // this.language.changeLang(this.cookie.get('lang') || 'en');
-    this.language.changeLang('en');
-    if (isPlatformBrowser(this.platform)) {
-      console.warn('browser');
-      // Use document
-    }
-    if (isPlatformServer(this.platform)) {
-      console.warn('server');
-      // Use document injected (this.document)
-    }
+    this.language.changeLang((this.cookie.get('lang') as DT_LANGS) || 'en');
+    // if (isPlatformBrowser(this.platform)) {
+    //   console.warn('browser');
+    //   // Use document
+    // }
+    // if (isPlatformServer(this.platform)) {
+    //   console.warn('server');
+    //   // Use document injected (this.document)
+    // }
   }
 }
--- src/app/shared/services/language/language.service.ts
+++ src/app/shared/services/language/language.service.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
 export type DT_LANGS = 'en' | 'es' | 'pt';
 
@@ -7,13 +8,14 @@ export type DT_LANGS = 'en' | 'es' | 'pt';
   providedIn: 'root',
 })
 export class LanguageService {
-  // private readonly cookie = inject(SsrCookieService);
+  private readonly cookie = inject(SsrCookieService);
   private readonly translate = inject(TranslateService);
-  public currentLang: DT_LANGS = 'en';
   public changeLang(lang: DT_LANGS): void {
     this.translate.use(lang);
     this.translate.setDefaultLang(lang);
-    this.currentLang = lang;
-    // this.cookie.set('lang', lang);
+
+    const expires = new Date();
+    expires.setFullYear(expires.getFullYear() + 100);
+    this.cookie.set('lang', lang, { expires });
   }
 }
