c0be3d98cdaaad576dcc9543eec931082619823a
rdevgc <rubengc1799@gmail.com>
2025-01-25 17:56:16 +0100

    DEV-153: HEADER REQUEST USER PROFILE WHEN ROUTE CHANGES

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 import {
   DtSelectButtonComponent,
   SelectButtonOptions,
 } from '@shared/components/dt-select-button/dt-select-button.component';
-import { filter, map, Observable } from 'rxjs';
+import { filter, map, Observable, switchMap } from 'rxjs';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { DtUserMenuComponent } from './components/user-menu/user-menu.component';
 
@@ -23,9 +24,16 @@ import { DtUserMenuComponent } from './components/user-menu/user-menu.component'
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  //#region SERVICES
+  //#region USER PROFILE
   private readonly router = inject(Router);
-  //#endregion SERVICES
+  private readonly authService = inject(AuthService);
+  private onRouteChange$ = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+  );
+  public userProfile$ = this.onRouteChange$.pipe(
+    switchMap(() => this.authService.userProfile$),
+  );
+  //#endregion USER PROFILE
 
   //#region LEFT BUTTONS
   public readonly LEFT_BUTTONS_OPTIONS: { id: string; label: string }[] = [
