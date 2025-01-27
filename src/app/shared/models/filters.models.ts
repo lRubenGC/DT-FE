528e45aeb652ff4e42d8eafe3c5167935889b034
rdevgc <rubengc1799@gmail.com>
2025-01-27 11:41:17 +0100

    DEV-170: SHARED MODELS

--- src/app/shared/models/filters.models.ts
+++ src/app/shared/models/filters.models.ts
@@ -3,3 +3,7 @@ export enum USER_PROPERTY {
   WISHED = 'wished',
   NOT_OWNED = 'not_owned',
 }
+
+export type CAR_TYPE = 'basic' | 'premium' | 'special';
+
+export type ORDER_TYPE = 'ASC' | 'DESC';
